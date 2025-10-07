6974d64d7ef87910fb239dd782e6cde87e67d154
fanyicharllson <fanyicharllson1@gmail.com>
2025-10-06 17:59:06 -0700

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-3xl text-blue-400 font-bold'>Learning react native</Text>
+      <Text className='italic font-semibold text-4xl'>
+        wow
+      </Text>
+    </View>
+  )
+}
+
+export default Index
+
+const styles = StyleSheet.create({})
